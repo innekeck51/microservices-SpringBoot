31dc5ea6d0cf92637eb711195252a8753741b3f6
inneke <innekeck51@gmail.com>
2021-09-26 12:00:33 +0700

    microservices

---
+++ department-service/src/main/java/com/gamabox/department/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.gamabox.department.service;
+
+import com.gamabox.department.entity.Department;
+import com.gamabox.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
---
+++ user-service/src/main/java/com/gamabox/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gamabox.user.repository;
+
+import com.gamabox.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
